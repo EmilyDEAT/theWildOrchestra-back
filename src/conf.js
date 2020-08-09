9554014bfa6ca9403b178574b87b5be00daab77a
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-08-09 12:15:41 +0200

    Replace createConnection w/ createPool and promisify query

--- src/conf.js
+++ src/conf.js
@@ -1,17 +1,14 @@
 const mysql = require('mysql')
+const { promisify } = require('util')
 require('dotenv').config()
-const loggers = require('./helpers/loggers')
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_NAME
 })
 
-connection.connect((err) => {
-  if (err) throw err
-  loggers.mysql('connected !')
-})
+pool.queryAsync = promisify(pool.query.bind(pool))
 
-module.exports = connection
+module.exports = pool
